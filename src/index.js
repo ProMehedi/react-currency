18e605e69cec0ac35be5430e76d580e0b004820e
promehedi <mmhasan563@gmail.com>
2021-09-05 17:07:50 +0600

    Fix Deploy Issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom'
 import App from './App'
-import 'currency-flags/dist/currency-flags.min.css'
 import 'semantic-ui-css/semantic.min.css'
 import { GlobalStyle } from './App.style'
 
