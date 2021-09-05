c65f39b427e6d5a949c09ae997e7a6cd546f682f
promehedi <mmhasan563@gmail.com>
2021-09-05 10:51:52 +0600

    Test Exchange API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
+  const [rates, setRates] = useState({})
+
+  const getRates = async () => {
+    const { data } = await axios.get(process.env.REACT_APP_API_URL)
+    setRates(data)
+  }
+
+  useEffect(() => {
+    getRates()
+  }, [])
+
+  console.log(rates)
+
   return (
     <div>
       <h1>Ready to go</h1>
